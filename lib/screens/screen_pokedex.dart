b6e78f843dd79dbad9f46223433e4c4cb198143b
pillow <hoseungbin@kaist.ac.kr>
2026-01-13 14:50:22 +0900

    cleaning up imports 2

--- lib/screens/screen_pokedex.dart
+++ lib/screens/screen_pokedex.dart
@@ -4,9 +4,8 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import '../models/models.dart';
-import '../providers/pokedex_provider.dart';
-import '../providers/diary_provider.dart';
-import '../screens/popup_pokemon_detail.dart';
+import '../providers/providers.dart';
+import 'screens.dart';
 
 class Tab3Pokedex extends StatefulWidget {
   const Tab3Pokedex({super.key});
